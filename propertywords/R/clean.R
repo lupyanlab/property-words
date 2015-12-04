ae999295095e492f4a1794a5e8d4841a5a1b990f
Pierce Edmiston <pierce.edmiston@gmail.com>
2015-12-03 22:26:04 -0600

    Use timeout trials

--- propertywords/R/clean.R
+++ propertywords/R/clean.R
@@ -6,7 +6,7 @@ clean <- function(frame) {
       rt = ifelse(is_correct, rt, NA),
 
       # Drop accuracy on timeout trials
-      is_correct = ifelse(response == "timeout", NA, is_correct),
+      # is_correct = ifelse(response == "timeout", NA, is_correct),
 
       # Create is_error from is_correct
       is_error = ifelse(is_correct, 0, 1)
